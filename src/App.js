4f4216b4553a2dc4098cc8c3a6939fe34d99dc91
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-04-13 23:54:38 -0500

    added jquery and popper.js for bootstrap to work

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Bootstrap from "bootstrap";
 
 function App() {
   return (
     <div className="App">
         <h1>Gabe's Portfolio</h1>
+
+        <div className="p-5">
+            does it work
+        </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,4 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+// import $ from 'jquery';
+// import Popper from 'popper.js';
+// import 'bootstrap/dist/js/bootstrap.bundle.min';
+
 ReactDOM.render(<App />, document.getElementById("root"));
