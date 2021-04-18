6bd41b713987985a3755798157f5f409cf2c3230
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-04-18 15:36:27 -0500

    added contact and resume components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
 import "./App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+  Link,
+  useRouteMatch,
+} from "react-router-dom";
 import NavBar from "../src/components/NavBar";
 import MainInfo from "../src/components/MainInfo";
 import MainImage from "../src/components/MainImage";
@@ -8,29 +16,35 @@ import faker from "faker";
 function App() {
   let randomName = faker.name.findName();
   return (
-    <div className="homepage-section">
-      <div className="img-overlay">
-        <div className="App container">
-          <NavBar />
+    <>
+      <Router>
+        <Link to="/">Home</Link>
+        <Route path="/" component={Home} />
+      </Router>
+      <div className="homepage-section">
+        <div className="img-overlay">
+          <div className="App container">
+            <NavBar />
 
-          <div className="row mt-5">
-            <div className="col-md-6 col-sm-6">
-              <MainInfo />
-            </div>
+            <div className="row mt-5">
+              <div className="col-md-6 col-sm-6">
+                <MainInfo />
+              </div>
 
-            {/* <div className="col-md-2 "></div> */}
-            <div className="col-md-6 col-sm-6">
-              <MainImage />
-            </div>
+              {/* <div className="col-md-2 "></div> */}
+              <div className="col-md-6 col-sm-6">
+                <MainImage />
+              </div>
 
-            {/* put this on resume page */}
-            {/* <div className="col-md-4 ">
+              {/* put this on resume page */}
+              {/* <div className="col-md-4 ">
             <JobInfoJson />
           </div> */}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
