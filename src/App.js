a30663db4847f92225b48fa42dd91ff9dd1e3917
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-04-25 20:20:20 -0500

    added break points to have main info and main img to show on same row

--- src/App.js
+++ src/App.js
@@ -69,43 +69,74 @@ class App extends Component {
             </div>
           </div>
         </nav>
-
         {/* end the navBar  */}
-
         {/* start mainInfo section  */}
-        <div className="container mb-3">
-          <div className="mb-2" id="ownerName">
-            GABRIEL AUSTIN
-            <hr className="mainPageMiddleHr" />
-          </div>{" "}
-          <br />
-          <div id="ownerNextLevel">
-            <span className="specialText">NEXT LEVEL</span>
-          </div>{" "}
-          <br />
-          <div id="ownerTitle">WEB DEVELOPER</div> <br />
-          <div className="mb-3" id="ownerDesc">
-            <p>
-              I am an I.T. Professional with the{" "}
-              <span className="specialText">superpower</span> to communicate
-              technical information to non-technical audiences.
-            </p>
-            {/* move this section to about or resume section */}
-            {/* <span className="specialText">Current</span> career path: <br />
-            <hr className="careerPathHr" />
-            IT - Banking - Education - IT <br />
-            <span className="specialText">Future</span> career path: <br />
-            <hr className="careerPathHr" />
-            Web Developer - Scrum Master - Project Manager */}
+        <div className="row">
+          <div className="col-md-6">
+            <div className="container mb-3">
+              <div className="mb-2" id="ownerName">
+                GABRIEL AUSTIN
+                <hr className="mainPageMiddleHr" />
+              </div>{" "}
+              <br />
+              <div id="ownerNextLevel">
+                <span className="specialText">NEXT LEVEL</span>
+              </div>{" "}
+              <br />
+              <div id="ownerTitle">WEB DEVELOPER</div> <br />
+              <div className="mb-3" id="ownerDesc">
+                <p>
+                  I am an I.T. Professional with the{" "}
+                  <span className="specialText">superpower</span> to communicate
+                  technical information to non-technical audiences.
+                </p>
+              </div>
+              {/* move this section to about or resume section */}
+              <div className="d-grid gap-3 col-md-6 mx-auto">
+                <button type="button" class="btn btn-warning py-2 px-5">
+                  My Projects
+                </button>
+              </div>
+            </div>
+            {/* end mainInfo section  */}
           </div>
-          {/* move this section to about or resume section */}
-          <div className="d-grid gap-3 col-md-6 mx-auto">
-            <button type="button" class="btn btn-warning py-2 px-5">
-              My Projects
-            </button>
+
+          <div className="col-md-6">
+            {/* start mainImage section  */}
+            <div className="margin-auto text-center">
+              <img
+                src={superHeroMe}
+                alt="Gabriel Austin"
+                className="rounded-circle w-50"
+              />
+            </div>
+
+            <div className="mb-3 text-center" id="ownerDesc">
+              <div>
+                Meet Gabe Austin
+                <br />
+                Future Web Developer &#64; <br />
+                <span className="jobTitle">
+                  <span className="specialText">&#65371;</span> yourCompanyName{" "}
+                  <span className="specialText">&#65373;</span>
+                </span>
+              </div>
+              {/* <hr className="mainPageMiddleHr" />
+          <div>
+            HTML <span className="specialText">|</span> CSS{" "}
+            <span className="specialText">|</span> JavaScript{" "}
+            <span className="specialText">|</span> Ruby{" "}
+            <span className="specialText">|</span> Version Control (Git){" "}
+            <span className="specialText">|</span> Agile Web Development{" "}
+            <span className="specialText">|</span> Scrum{" "}
+            <span className="specialText">|</span> Project Management{" "}
+            <span className="specialText">|</span> Training/Teaching
+          </div> */}
+            </div>
+            {/* end mainImage section */}
           </div>
         </div>
-        {/* end mainInfo section  */}
+        {/* end the row on line 75 */}
       </>
     );
   }
