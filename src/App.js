3dabeb81c3b68f78cbb5d01608014beee6cbeba7
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-04-25 19:00:19 -0500

    fixed navbar container issue where navbar doesnt expand full div

--- src/App.js
+++ src/App.js
@@ -11,20 +11,65 @@ class App extends Component {
     return (
       <>
         {/* put the navBar here  */}
-        <nav class="navbar navbar-light bg-light">
-          <div class="container-fluid">
-            <a class="navbar-brand" href="#">
-              <img
-                src={cartoonMePeace}
-                alt="Gabriel Austin"
-                className="rounded-circle w-50"
-                class="d-inline-block align-text-top"
-                id="nav-bar-img"
-              />
-              Gabe Austin
-            </a>
+        <nav
+          class="navbar navbar-expand-lg navbar-brand bg-transparent"
+          id="main-navbar"
+        >
+          <div class="container-fluid transparent" id="nav-container">
+            <img
+              src={cartoonMePeace}
+              alt="Gabriel Austin"
+              className="rounded-circle w-50"
+              class="d-inline-block align-text-top "
+              id="nav-bar-img"
+            />
+
+            <button
+              class="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                  <a class="nav-link" href="#" id="nav-link-home">
+                    Home
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link nav-link-css" href="#" id="nav-link-about">
+                    About
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a
+                    class="nav-link nav-link-css"
+                    href="#"
+                    id="nav-link-resume"
+                  >
+                    Resume
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a
+                    class="nav-link nav-link-css"
+                    href="#"
+                    id="nav-link-contact"
+                  >
+                    Contact
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
         </nav>
+
         {/* end the navBar  */}
 
         {/* start mainInfo section  */}
