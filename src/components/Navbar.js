affbd2e68cf90ad8d95293d06eb6d018e21639fe
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-05-06 19:05:57 -0500

    added links and pages to the nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ class Navbar extends Component {
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
           <a class="navbar-brand" href="#">
-            Navbar
+            myBoyGabe
           </a>
           <button
             class="navbar-toggler"
@@ -22,15 +22,25 @@ class Navbar extends Component {
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0 m-auto">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">
-                  Home
+                <a class="nav-link" href="#">
+                  About
                 </a>
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="#">
-                  Link
+                  Resume
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Projects
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Contact
                 </a>
               </li>
             </ul>
