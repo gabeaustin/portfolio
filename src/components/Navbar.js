a50fa93907381193fd4edf5ef44187e514cdae50
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-06-08 15:37:05 -0500

    added transition to the CTA contact button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,48 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Button } from "reactstrap";
 import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    // <div>
-    //   <nav className="navbar navbar-inverse navbar-fixed-top">
-    //     {/* <div className="logo-text">MyBoyGabe</div> */}
-    //     <button
-    //       className="navbar-toggler"
-    //       type="button"
-    //       data-toggle="collapse"
-    //       data-target="#navbarSupportedContent"
-    //       aria-controls="navbarSupportedContent"
-    //       aria-expanded="false"
-    //       aria-label="Toggle navigation"
-    //     >
-    //       <span className="navbar-toggler-icon"></span>
-    //     </button>
-
-    //     <ul className="nav nav-links text-center">
-    //       {/* <li className="nav-item">
-    //         <Link to="/about">About</Link>
-    //       </li> */}
-
-    //       <li className="nav-item m-2">
-    //         <Link to="/comingsoon">Projects</Link>
-    //       </li>
-
-    //       {/* uncomment /projects once projects page is completed  */}
-    //       {/* <Link to="/projects">Projects</Link> */}
-    //       <li className="nav-item m-2">
-    //         <Link to="/contact">Contact</Link>
-    //       </li>
-
-    //       {/* <li className="nav-item">
-    //         <Link to="/resume">Resume</Link>
-    //       </li> */}
-    //     </ul>
-    //   </nav>
-    // </div>
-
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg">
       <div className="container-fluid">
         <Link to="/" className="navbar-brand logo-text">
           myBoyGabe
@@ -72,9 +34,8 @@ const Navbar = () => {
               </Link>
             </li>
           </ul>
-          <span className="navbar-text">
-            <button class="btn btn-outline-warning">Contact</button>
-          </span>
+
+          <button class="btn btn-outline-warning contact-cta-btn">Contact</button>
         </div>
       </div>
     </nav>
