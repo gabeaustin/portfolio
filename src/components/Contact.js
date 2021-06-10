f03ee390c13ae0b894a828aeece2945773ce9ca1
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-06-10 02:07:19 -0500

    started work on refactoring contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,45 +1,45 @@
 import React from "react";
-import "../Contact.css";
-
-import { Link } from "react-router-dom";
-import { Button } from "reactstrap";
 
 const Contact = () => {
-  {
-    /*
-    Symbols Reference
-    
-    &#58;   ->  colon( : )
-    &#123;  ->  opening curly bracket( { )
-    &#125;  ->  closing culry bracket( } )
-    
-    */
-  }
-
   return (
-    <div className="container contact-page-div h2">
-      <span className="yellow">&#123;</span>
-      <br />
-      <span className="blue mx-4">"name"</span>
-      <span className="grey">&#58;</span>
-      <span className="green">"Gabriel Austin"</span>
-      <span className="grey">,</span>
-      <br />
-      <span className="blue mx-4">"email"</span>
-      <span className="grey">&#58;</span>{" "}
-      <span className="green">"gabriel.austin2019@gmail.com"</span>
-      <span className="grey">,</span>
-      <br />
-      <span className="blue mx-4">"github"</span>
-      <span className="grey">&#58;</span>{" "}
-      <span className="green">"www.github.com/gabeaustin"</span>
-      <span className="grey">,</span>
-      <br />
-      <span className="blue mx-4">"linkedin"</span>
-      <span className="grey">&#58;</span>{" "}
-      <span className="green">"www.linkedin.com/in/gabrielaustin"</span>
-      <br />
-      <span className="yellow">&#125;</span>
+    <div className="container">
+      <h1>Contact Me</h1>
+
+      <form>
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            class="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+          />
+          <div id="emailHelp" class="form-text">
+            We'll never share your email with anyone else.
+          </div>
+        </div>
+        <div class="mb-3">
+          <label for="exampleInputPassword1" class="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            class="form-control"
+            id="exampleInputPassword1"
+          />
+        </div>
+        <div class="mb-3 form-check">
+          <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+          <label class="form-check-label" for="exampleCheck1">
+            Check me out
+          </label>
+        </div>
+        <button type="submit" class="btn btn-primary">
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
