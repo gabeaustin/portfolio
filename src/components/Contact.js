f154f4eaa8ec05518d35f8bf70ac1b26a39b18d1
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-06-11 05:51:07 -0500

    added downloadable resume pdf and tooltip

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,39 +1,53 @@
 import React from "react";
 
+import { Link } from "react-router-dom";
+import { Button } from "reactstrap";
+import { FaEnvelope, FaGithub, FaLinkedin, FaFile } from "react-icons/fa";
+
+import resume from "../pdfs/resume.pdf";
+
 const Contact = () => {
   return (
     <div className="container">
-      <h1 className="mb-3 contact-heading">Contact Me</h1>
-
-      <form>
-        <div className="row">
-          <div class="mb-5 col-md-6">
-            <label for="name" class="form-label fs-5">
-              Name
-            </label>
-            <input type="text" class="form-control" id="name1" />
-          </div>
-
-          <div class="mb-5 col-md-6">
-            <label for="email" class="form-label fs-5">
-              Email
-            </label>
-            <input type="email" class="form-control" id="email1" />
-          </div>
+      <h1 className="mb-4 contact-heading">Contact Me</h1>
+      <div className="row">
+        <div className="h1 mb-2 d-flex col-md-12">Socials</div>
+        <div className="d-flex col-md-12">
+          <a href="mailto:gabriel.austin2019@gmail.com">
+            <FaEnvelope className="mainPageFooterIcons contact-icons mx-4" />
+          </a>
+          <a href="https://github.com/gabeaustin" target="_blank">
+            <FaGithub className="mainPageFooterIcons contact-icons mx-4" />
+          </a>
+          <a href="https://www.linkedin.com/in/gabrielaustin/" target="_blank">
+            <FaLinkedin className="mainPageFooterIcons contact-icons mx-4" />
+          </a>
         </div>
 
-        <div class="mb-3">
-          <label for="message" class="form-label fs-5">
-            Message
-          </label>
-          {/* <input type="textarea" class="form-control" id="message1" /> */}
-          <textarea class="form-control" id="message1" rows="3"></textarea>
+        <div className="h1 mb-2">Resume</div>
+        <div className="mb-4">
+          <a href={resume} target="_blank">
+            <FaFile
+              className="mainPageFooterIcons contact-icons mx-4"
+              data-bs-toggle="tooltip"
+              data-bs-placement="right"
+              title="Click to Download"
+            />
+          </a>
         </div>
+      </div>
 
-        <button type="submit" class="btn btn-warning mb-5">
-          Submit
-        </button>
-      </form>
+      {/* <div className="col-md-4 fs-4 col-md-4 col-sm-12 col-xs-12">
+        gabriel.austin2019@gmail.com
+      </div> */}
+
+      <div className="d-grid col-md-6 col-sm-12 mx-auto text-center mt-5">
+        <Link to="/">
+          <Button color="warning" className="is-rounded">
+            <span className="home-button">Home</span>
+          </Button>
+        </Link>
+      </div>
     </div>
   );
 };
