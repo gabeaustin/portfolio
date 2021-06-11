54c2ab570c46bc4a14098e7b809457de59db606b
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-06-11 15:16:08 -0500

    fixed media queries for resume pg - pdf will not show on mobile

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -10,36 +10,44 @@ import PDF from "../pdfs/resume.pdf";
 
 const Resume = () => {
   return (
-    <div className="container mt-3 pt-3">
-      <div className="row">
-        <div className="mb-4 h1 resume-heading row justify-content-center">
-          Resume
+    <>
+      <div className="container mt-3 pt-3 no-show-on-mobile">
+        <div className="row">
+          <div className="mb-4 h1 resume-heading row justify-content-center">
+            Resume
+          </div>
+
+          <div className="h3 row justify-content-center">Click to Download</div>
+
+          <div className="row justify-content-center mb-4">
+            <div className="h1 justify-content-center col-md-4 col-lg-4">
+              <FaHandPointRight className="svg-hand-point col-md-4 col-lg-4" />
+              <a href={resume} target="_blank">
+                <FaFile
+                  className="mainPageFooterIcons mx-4"
+                  data-bs-toggle="tooltip"
+                  data-bs-placement="right"
+                  title="Click to Download"
+                />
+              </a>
+              <FaHandPointLeft className="svg-hand-point col-md-4 col-lg-4" />
+            </div>
+          </div>
         </div>
 
-        <div className="h3 row justify-content-center no-show-on-mobile">
-          Click to Download
+        <div className="container-fluid d-none d-lg-block mb-5 pb-5 resume-viewer-container">
+          <Viewer fileUrl={PDF} />
         </div>
+      </div>
 
-        <div className="row justify-content-center mb-4 no-show-on-mobile">
-          <div className="h1 justify-content-center col-md-4 col-lg-4">
-            <FaHandPointRight className="svg-hand-point col-md-4 col-lg-4" />
-            <a href={resume} target="_blank">
-              <FaFile
-                className="mainPageFooterIcons mx-4"
-                data-bs-toggle="tooltip"
-                data-bs-placement="right"
-                title="Click to Download"
-              />
-            </a>
-            <FaHandPointLeft className="svg-hand-point col-md-4 col-lg-4" />
+      <div className="container mt-3 pt-3 no-show-on-computer">
+        <div className="row">
+          <div className="mb-4 h1 resume-heading row justify-content-center">
+            Resume
           </div>
         </div>
       </div>
-
-      <div className="container-fluid d-none d-lg-block mb-5 pb-5 resume-viewer-container">
-        <Viewer fileUrl={PDF} />
-      </div>
-    </div>
+    </>
   );
 };
 
