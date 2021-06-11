0741a6a5282579e4bc0074fca5881cc51ec78067
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-06-11 13:02:53 -0500

    added bootstrap className so svg icons on resume page wont show on mobile

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -11,28 +11,28 @@ import PDF from "../pdfs/resume.pdf";
 const Resume = () => {
   return (
     <div className="container mt-3 pt-3">
-      {/* <div className="d-flex px-3"> */}
       <div className="row">
         <div className="mb-4 h1 resume-heading row justify-content-center">
           Resume
         </div>
-        {/* </div> */}
 
-        <div className="mb-4 h1 row justify-content-center">
+        <div className="h3 row justify-content-center d-none d-lg-block">
           Click to Download
         </div>
 
-        <div className="mb-4 h1 row justify-content-center border">
-          <FaHandPointRight className="svg-hand-point" />
-          <a href={resume} target="_blank">
-            <FaFile
-              className="mainPageFooterIcons contact-icons mx-4"
-              data-bs-toggle="tooltip"
-              data-bs-placement="right"
-              title="Click to Download"
-            />
-          </a>
-          <FaHandPointLeft className="svg-hand-point" />
+        <div className="row justify-content-center border mb-4 d-none d-lg-block">
+          <div className="h1 justify-content-center col-xs-4 col-sm-4 col-md-4 col-lg-4">
+            <FaHandPointRight className="svg-hand-point col-xs-4 col-sm-4 col-md-4 col-lg-4" />
+            <a href={resume} target="_blank">
+              <FaFile
+                className="mainPageFooterIcons mx-4"
+                data-bs-toggle="tooltip"
+                data-bs-placement="right"
+                title="Click to Download"
+              />
+            </a>
+            <FaHandPointLeft className="svg-hand-point col-xs-4 col-sm-4 col-md-4 col-lg-4" />
+          </div>
         </div>
       </div>
 
