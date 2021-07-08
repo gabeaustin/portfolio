2c1654eedb43cfa37e5fcf2d77de1314cd64ab05
gabeaustin <gabriel.austin2019@gmail.com>
2021-07-08 11:53:49 -0500

    added suspense and lazy loading to homepage to reduce load time

--- src/App.js
+++ src/App.js
@@ -1,49 +1,39 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { Worker } from "@react-pdf-viewer/core";
 import "../src/App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
-import HomePage from "./components/HomePage";
-import ComingSoon from "./components/ComingSoon";
-import Contact from "./components/Contact";
-import Projects from "./components/Projects";
-import Resume from "./components/Resume";
+const HomePage = lazy(() => import("./components/HomePage"));
 
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+// import HomePage from "./components/HomePage";
+// import ComingSoon from "./components/ComingSoon";
+// import Contact from "./components/Contact";
+// import Projects from "./components/Projects";
+// import Resume from "./components/Resume";
+
+// import Navbar from "./components/Navbar";
+// import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <BrowserRouter>
-      {/* This is for PDF viewer  */}
-      <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.6.347/build/pdf.worker.min.js"></Worker>
-      <Navbar />
-      <div className="App">
-        <Switch>
-          <Route exact path="/">
+      <Suspense fallback={<div>Page is Loading...</div>}>
+        {/* This is for PDF viewer  */}
+        <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.6.347/build/pdf.worker.min.js"></Worker>
+        {/* <Navbar /> */}
+        <div className="App">
+          <Switch>
+            {/* <Route exact path="/">
             <HomePage />
-          </Route>
+          </Route> */}
 
-          <Route exact path="/contact">
-            <Contact />
-          </Route>
-
-          <Route exact path="/projects">
-            <Projects />
-          </Route>
-
-          <Route exact path="/resume">
-            <Resume />
-          </Route>
-
-          <Route exact path="/comingsoon">
-            <ComingSoon />
-          </Route>
-        </Switch>
-      </div>
-      <Footer />
+            <Route exact path="/" component={HomePage} />
+          </Switch>
+        </div>
+      </Suspense>
+      {/* <Footer /> */}
     </BrowserRouter>
   );
 };
