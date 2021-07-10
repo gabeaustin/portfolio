67201a5285de7316c37e12e34045de2f875cdfd7
gabeaustin <gabriel.austin2019@gmail.com>
2021-07-10 14:17:15 -0500

    updated webpack config file to fix issue where a 404 shows up if page is reloaded

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ const App = () => {
             <Projects />
           </Route>
 
-          <Route path="/resume">
+          <Route path="/resume" exact>
             <Resume />
           </Route>
 
