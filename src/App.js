8d8f114ca4996e3e7bc5705862633f8d560c197c
gabeaustin <gabriel.austin2019@gmail.com>
2021-07-10 00:38:21 -0500

    removed lazy loading

--- src/App.js
+++ src/App.js
@@ -1,51 +1,48 @@
-import React, { Suspense, lazy } from "react";
+import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { Worker } from "@react-pdf-viewer/core";
 import "../src/App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
+import HomePage from "./components/HomePage";
 import ComingSoon from "./components/ComingSoon";
 import Contact from "./components/Contact";
 import Projects from "./components/Projects";
+import Resume from "./components/Resume";
+
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-
-
-{/* Lazy Loading for Site Optimization - lines below */}
-const HomePage = lazy(() => import("./components/HomePage"));
-const Resume = lazy(() => import("./components/Resume"));
 
 const App = () => {
   return (
     <BrowserRouter>
-      {/* This is for PDF viewer - line below  */}
+      {/* This is for PDF viewer  */}
       <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.6.347/build/pdf.worker.min.js"></Worker>
       <Navbar />
       <div className="App">
-        <Suspense fallback={<div>Page is Loading...</div>}>
-          <Switch>
-            <Route exact path="/" component={HomePage}>
-            </Route>
+        <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
 
-            <Route exact path="/contact">
-              <Contact />
-            </Route>
+          <Route exact path="/contact">
+            <Contact />
+          </Route>
 
-            <Route exact path="/projects">
-              <Projects />
-            </Route>
+          <Route exact path="/projects">
+            <Projects />
+          </Route>
 
-            <Route exact path="/resume" component={Resume}>
-            </Route>
+          <Route exact path="/resume">
+            <Resume />
+          </Route>
 
-            <Route exact path="/comingsoon">
-              <ComingSoon />
-            </Route>
-          </Switch>
-        </Suspense>
+          <Route exact path="/comingsoon">
+            <ComingSoon />
+          </Route>
+        </Switch>
       </div>
-
       <Footer />
     </BrowserRouter>
   );
